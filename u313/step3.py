3c07f89610d6493d4ec0c534b68380e67928d502
R. Cruz <roberto.cruz@vtsd.com>
2024-03-01 12:29:29 -0500

    example

---
+++ u313/step3.py
@@ -0,0 +1,8 @@
+x = input("Please select a sandwhich type")
+if x == "Chicken":
+    print("Chicken")
+
+y = input("Please select a beverage")
+
+print("Sandwhich type:" , x)
+print("Beverage type:" , y)
